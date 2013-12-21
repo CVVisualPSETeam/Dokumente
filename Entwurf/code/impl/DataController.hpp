26810f409d06601ce2471505ddb5d9f5e2290ff6
Florian Weber <florian.weber@sfz-bw.de>
2013-12-21 17:03:14 +0100

    improve DataController and add refernce(helper)

--- Entwurf/code/impl/DataController.hpp
+++ Entwurf/code/impl/DataController.hpp
@@ -16,6 +16,17 @@ public:
 	DataController() { /* implement */ }
 	
 	void addCall(std::unique_ptr<Call> call) {calls.push_back(std::move(call));}
+	void removeCall(size_t Id);
+	const Call& getCall(size_t Id) const;
+	Call& getCall(size_t Id);
+	
+	/**
+	 * Passes control to the View-controller.
+	 * 
+	 * Returns when the ViewController signals that normal program-execution shall continue.
+	 * 
+	 */
+	void callUI();
 	
 private:
 	WindowController windowController;
